0dcdf475018ee1ec514609440620ffbc9ffd9b11
Luciano Alves <luas10c@gmail.com>
2023-12-02 22:44:09 -0300

    feat: add page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { UploadFile } from '#/components/upload-file'
+
 const Home = () => {
   return (
-    <div>
-      <h2>Home!</h2>
-    </div>
+    <section className="mx-auto w-full max-w-[600px] px-4">
+      <header className="py-4 text-center">
+        <h2 className="text-2xl font-bold text-zinc-300">Imgur Uploader</h2>
+        <p className="text-zinc-300">
+          Upload your image below and directly receive a link for it.
+        </p>
+      </header>
+      <UploadFile />
+    </section>
   )
 }
 
